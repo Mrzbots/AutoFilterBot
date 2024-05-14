afcdbf0b82235f157a1995557ba3a79cf9474a18
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-14 19:56:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,6 +40,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'xbotsupports')
+UPDATE_CHANNEL = environ.get('UPDATE_CHANNEL', 'botuptest')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
