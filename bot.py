ee4e104eea0b6302d1894d5d3dc9e211b5f36701
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-15 13:13:39 +0530

    Restart message added

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
+from Script import script
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, PORT, LOG_CHANNEL
 from utils import temp
