71c4a8cc7e0eab40ca95ad090b27519608a06946
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-16 20:36:09 +0530

    Fixed

--- plugins/group_manage/pin.py
+++ plugins/group_manage/pin.py
@@ -3,34 +3,33 @@ from pyrogram import *
 
 @Client.on_message(filters.command("unpin_all") & filters.group)
 async def unpinall(client, message: Message):
-    try:
-        user = await client.get_chat_member(message.chat.id, message.from_user.id)
-        bot_stats = await client.get_chat_member(message.chat.id, "self")
-        if not bot_stats.status.can_change_info:
-            return await message.reply("Iam not Admin 😞")
-        elif bot_stats.status.can_unpin_all_chat_messages and not message.reply_to_message:
-            return await message.reply("Sorry dude I don't have pin rights 🙃")
-        elif user.status.can_unpin_all_chat_messages and not message.reply_to_message:
-            return await message.reply("you are admin this chat but you don't have pin rights")
-        elif not user.status:
-            return await message.reply("Sorry dude you dont have permission ")
-        await client.unpin_all_chat_messages(message.chat.id)
-    except Exception as e:
-        await message.reply_text(f"{e}")
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
+    bot_stats = await client.get_chat_member(message.chat.id, "self")
+    if not bot_stats.status.can_change_info:
+        return await message.reply("Iam not Admin 😞")
+    elif bot_stats.status.can_unpin_all_chat_messages and not message.reply_to_message:
+        return await message.reply("Sorry dude I don't have pin rights 🙃")
+    elif user.status.can_unpin_all_chat_messages and not message.reply_to_message:
+        return await message.reply("you are admin this chat but you don't have pin rights")
+    elif not user.status:
+        return await message.reply("Sorry dude you dont have permission ")
+    await client.unpin_all_chat_messages(message.chat.id)    
 
+
+
+        
 @Client.on_message(filters.command("pin") & filters.group)
 async def pin_handler(client, message: Message):
-    try:
-        user = await client.get_chat_member(message.chat.id, message.from_user.id)
-        bot_stats = await client.get_chat_member(message.chat.id, "self")
-        if not bot_stats.status.can_change_info:
-            return await message.reply("Iam not Admin 😞")
-        elif bot_stats.status.can_pin_messages and not message.reply_to_message:
-            return await message.reply("Sorry dude I don't have pin rights 🙃")
-        elif user.status.can_pin_messages and not message.reply_to_message:
-            return await message.reply("you are admin this chat but you don't have pin rights")
-        elif not user.status:
-            return await message.reply("Sorry dude you dont have permission ")
-        await client.pin_chat_message(message.chat.id, msg)
-    except Exception as e:
-        await message.reply_text(f"{e}")
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
+    bot_stats = await client.get_chat_member(message.chat.id, "self")
+    if not bot_stats.status.can_change_info:
+        return await message.reply("Iam not Admin 😞")
+    elif bot_stats.status.can_pin_messages and not message.reply_to_message:
+        return await message.reply("Sorry dude I don't have pin rights 🙃")
+    elif user.status.can_pin_messages and not message.reply_to_message:
+        return await message.reply("you are admin this chat but you don't have pin rights")
+    elif not user.status:
+        return await message.reply("Sorry dude you dont have permission ")
+    await client.pin_chat_message(message.chat.id, msg)
+    
+        
