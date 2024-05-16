a21185ff886a953c23fa7833b07e795c34bc8b3c
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-05-16 11:22:22 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from info import CHANNELS
+from info import *
 from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
