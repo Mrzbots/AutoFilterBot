58e55099069092e2dfd5ebd26106588ba74a5123
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-16 17:27:27 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -12,9 +12,6 @@ logger.setLevel(logging.INFO)
 lock = asyncio.Lock()
 
 
-
-
-
 @Client.on_callback_query(filters.regex(r'^index'))
 async def index_files(bot, query):
     if query.data.startswith('index_cancel'):
