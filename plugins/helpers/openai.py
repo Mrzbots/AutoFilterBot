63d48051101fc174446724a023f3373b8d16edef
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-08-10 15:07:26 +0530

    done
    Free gpt

--- plugins/helpers/openai.py
+++ plugins/helpers/openai.py
@@ -1,22 +1,30 @@
 # credits @Mrz_bots
 
 import requests
-from HorridAPI import api 
+from HorridAPI.AiGenerativeContent import AiGenerativeContent
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command("ask"))
-async def ask(client, message):    
+openai = AiGenerativeContent.gen_content
+
+@Client.on_message(filters.command("openai"))
+async def openai(client, message):    
     if len(message.command) < 2:
         return await message.reply_text("Please provide query!")
     
     query = " ".join(message.command[1:])
     thinking_message = await message.reply_text("<b>wait...😎</b>")
+    payload = {
+        "messages": [                    
+            {            
+                "role": "user", 
+                "content": query}
+        ]
+    }
     try:        
-        response = api.llama(query)        
+        response = openai(payload, 5)        
         await thinking_message.edit(f"Hey {message.from_user.mention},\n\nQuery: {query}\n\nResult:\n\n<code>{response}</code>")
 
     except Exception as e:  
         # print(e)
         error_message = f"Hmm, something went wrong: {str(e)}"[:100] + "...\n use /bug comment"
         await thinking_message.edit(error_message)
-        
