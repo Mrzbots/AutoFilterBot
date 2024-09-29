fb88121f26c5d1903411028d5ca85b19334a8ea3
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-09-29 09:46:38 +0530

    Update openai.py

--- plugins/helpers/openai.py
+++ plugins/helpers/openai.py
@@ -1,35 +1,39 @@
 # credits @Mrz_bots
 
 import requests
-from HorridAPI.AiGenerativeContent import AiGenerativeContent 
+from MangoSeed import Mseed
+from info import DATABASE_URI
 from pyrogram import Client, filters
 
+mongo_url = DATABASE_URI
 
 @Client.on_message(filters.command("openai"))
 async def openai(client, message):
     text = " ".join(message.command[1:])
     if len(message.command) < 2:
-        return await message.reply_text("Please provide query!")
+        return await message.reply_text("Please provide a query!")
+    
     if message.reply_to_message:
-        query = f"{message.reply_to_message.text}\n{text}"
+        query = f"Old conversation: {message.reply_to_message.text}\nNew Conversation: {query}"
     else:
-        query = " ".join(message.command[1:])
+        query = text
+
     mes = await message.reply_text("🔍")
-    payload = {
-        "messages": [                    
-            {            
-                "role": "user", 
-                "content": query
-            }
-        ]
-    }
-    try:    
-        openai = AiGenerativeContent
-        response = openai.gen_content(payload, "gpt-3.5")
-        content = response['response']
+    k = Mseed(mongo_url)
+
+    try:
+        response = k.generate(
+            system="assistant",
+            prompt=query,
+            user_id=message.from_user.id,
+            model="gpt-3.5"
+        )
+        content = response['result']
         await mes.edit(f"Hey {message.from_user.mention},\n\nQuery: {text}\n\nResult:\n\n{content}")
 
-    except Exception as e:  
-        # print(e)
-        error_message = f"Hmm, something went wrong: {str(e)}"[:100] + "...\n use /bug comment"
+    except requests.exceptions.RequestException as e:
+        error_message = f"Error making request: {str(e)}"[:100] + "...\n use /bug comment"
+        await mes.edit(error_message)
+    except Exception as e:
+        error_message = f"Unknown error: {str(e)}"[:100] + "...\n use /bug comment"
         await mes.edit(error_message)
