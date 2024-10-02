64623cd721b9df01d0be15bb9636593ee9967d1d
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-10-02 14:28:30 +0530

    Telegraph changed to mangandi
    Telegraph changed to mangandi
    
    Bcs telegraph now not useful

--- plugins/helpers/telegraph.py
+++ plugins/helpers/telegraph.py
@@ -1,23 +1,56 @@
 from pyrogram import Client, filters
-from telegraph import upload_file
-import os
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from Mangandi import ImageUploader, VideoUploader
 
-@Client.on_message(filters.command('telegraph'))
-async def telegraph(client, message):
+@Client.on_message(filters.command(["mangandi", "telegraph"]))
+async def mgndipro(client, message):
     if message.reply_to_message:
-        if message.reply_to_message.photo or message.reply_to_message.video:
-            msg = await message.reply_text("Uploading to Telegraph...")
-            reply = message.reply_to_message
-            if message.reply_to_message.photo:
-                file = await reply.download()
-                file_name = file
-            else:
-                file = await reply.download()
-                file_name = file
-            response = upload_file((file_name, open(file, 'rb')))
-            await msg.edit(f"Telegraph Link: `https://telegra.ph{response[0]}`")
-            os.remove(file)
+        if message.reply_to_message.photo:
+            photo = message.reply_to_message.photo
+            if photo.file_size > 500 * 1024 * 1024:  # 500 MB
+                await message.reply("Image size is too large! Maximum allowed size is 500 MB")
+                return
+            s = await message.reply_text("`Downloading To My Server ...`")
+            await s.edit("`Downloading Completed. Now I am Uploading ...`")
+            k = await message.reply_to_message.download()
+            up = ImageUploader(k)
+            link = up.upload()
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton("Share Link", url=f"https://telegram.me/share/url?url={link}"),
+                 InlineKeyboardButton("Copy Link", url=f"{link}")]
+            ])            
+            await s.edit(f"**Link: {link}**\n\n**Copy link:** `{link}`\n\n>**By: @Mrz_bots**", reply_markup=keyboard)
+        elif message.reply_to_message.video:
+            video = message.reply_to_message.video
+            if video.file_size > 500 * 1024 * 1024:  # 500 MB
+                await message.reply("Video size is too large! Maximum allowed size is 500 MB")
+                return
+            s = await message.reply_text("`Downloading To My Server ...`")
+            await s.edit("`Downloading Completed. Now I am Uploading ...`")
+            k = await message.reply_to_message.download()
+            up = VideoUploader(k)
+            link = up.upload()
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton("Share Link", url=f"https://telegram.me/share/url?url={link}"),
+                 InlineKeyboardButton("Copy Link", url=f"{link}")]
+            ])            
+            await s.edit(f"**Link: {link}**\n\n**Copy link:** `{link}`\n\n>**By: @Mrz_bots**", reply_markup=keyboard)
+        elif message.reply_to_message.animation:
+            animation = message.reply_to_message.animation
+            if animation.file_size > 500 * 1024 * 1024:  # 500 MB
+                await message.reply("Gif size is too large! Maximum allowed size is 500 MB")
+                return
+            s = await message.reply_text("`Downloading To My Server ...`")
+            await s.edit("`Downloading Completed. Now I am Uploading ...`")
+            k = await message.reply_to_message.download()
+            up = VideoUploader(k)
+            link = up.upload()
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton("Share Link", url=f"https://telegram.me/share/url?url={link}"),
+                 InlineKeyboardButton("Copy Link", url=f"{link}")]
+            ])           
+            await s.edit(f"**Link: {link}**\n\n**Copy link:** `{link}`\n\n>**By: @Mrz_bots**", reply_markup=keyboard)
         else:
-            await message.reply_text("Please reply to a photo or video.")
+            await message.reply("Please reply to a photo or video or Gif")
     else:
-        await message.reply_text("Please reply to a message containing a photo or video.")
+        await message.reply("Please reply to a photo or video or Gif")
